bfd8ae2de982c67635f9556e601199945caaaf7f
Nikita <inik8808@gmail.com>
2023-04-18 14:11:45 +0500

    csvtest

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,24 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.VacationService;
 
 public class VacationServiceTest {
 
-    @Test
-    public void vacationWithLowWages() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/weekend.csv")
+    public void testWeekend(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void holidaysWithHighPay() {
-        VacationService service = new VacationService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
